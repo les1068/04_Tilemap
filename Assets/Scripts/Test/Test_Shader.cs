fa1d929a89d107203a0578131f25756991d5d285
les1068 <81298309+les1068@users.noreply.github.com>
2023-04-05 13:37:27 +0900

    230405-4 슬라임쉐이더 합치기

--- Assets/Scripts/Test/Test_Shader.cs
+++ Assets/Scripts/Test/Test_Shader.cs
@@ -5,7 +5,8 @@ using UnityEngine.InputSystem;
 
 public class Test_Shader : Test_Base
 {
-
+    public float acc = 0.0f;  // 누적시간
+    public float speed = 2.0f; 
     // 0번 : outline
     // 1,2번 : phase
     // 3,4번 : dissolve
@@ -34,8 +35,14 @@ public class Test_Shader : Test_Base
     {
         // 숫자를 0~1사이로 계속 왕복하게 만들기
         // phase와 dissolve가 계속 반복하기
-
+        acc += Time.deltaTime;
+        float num = (Mathf.Cos(acc * speed) + 1.0f) * 0.5f;
+        
         // 1,2번 : phase
+        materials[1].SetFloat("_Split", num);
+        materials[2].SetFloat("_Split", num);
         // 3,4번 : dissolve
+        materials[3].SetFloat("_Fade", num);
+        materials[4].SetFloat("_Fade", num);
     }
 }
