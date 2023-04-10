3a2f8366f3ad161b96a0416df97d9750d286876b
les1068 <81298309+les1068@users.noreply.github.com>
2023-04-10 10:51:19 +0900

    230410-2 타일맵 좌표 변경 함수 수정

--- Assets/Scripts/Astar/GridMap.cs
+++ Assets/Scripts/Astar/GridMap.cs
@@ -16,7 +16,7 @@ public class GridMap
 
     Tilemap background; // 배경용 타일맵 
 
-    public const int Error_Not_Vaild_Position = -1;  // 위치 입력이 잘못되었따는 것을 표시하기 위한 함수
+    public const int Error_Not_Vaild_Position = -1;  // 위치 입력이 잘못되었다는 것을 표시하기 위한 함수
 
     public GridMap(int width, int height)
     {
@@ -40,9 +40,36 @@ public class GridMap
     }
     public GridMap(Tilemap background, Tilemap obstacle)
     {
+        width = background.size.x;
+        height = background.size.y;
+
         // nodes 생성하기
-        // 원점 관련 처리 필요(GridToIndex 수정하기)
-        // 장애물 표시하기(Node의 gridType 벽으로 바꿔주기)
+        nodes = new Node[height * width];
+
+        origin = (Vector2Int)background.origin;
+
+        Vector2Int min = new(background.cellBounds.xMin, background.cellBounds.yMin);
+        Vector2Int max = new(background.cellBounds.xMax, background.cellBounds.yMax);
+
+        for (int y = min.y; y < max.y; y++)
+        {
+            for (int x = min.x; x < max.x; x++)
+            {
+                // 원점 관련 처리 필요(GridToIndex 수정하기)
+                int index = GridToIndex(x, y);
+                nodes[index] = new Node(x, y);
+
+                // 장애물 표시하기(Node의 gridType 벽으로 바꿔주기)
+                TileBase tile = obstacle.GetTile(new(x, y));
+                if(tile != null)
+                {
+                    Node node = GetNode(x, y);
+                    node.gridType = Node.GridType.Wall;
+                }
+            }
+        }
+        this.background = background;
+        
     }
 
     public Node GetNode(int x, int y) // 특정 위치에 있는 노드를 돌려주는 함수 / return x,y에 있는 노드
@@ -74,11 +101,19 @@ public class GridMap
     // 유틸리티 함수-----------------------------------------
     public Vector2Int WorldToGrid(Vector3 worldPos) // 월드 좌표를 그리드 좌표로 변경해주는 함수 / worldPos : 월드 좌표, return : 변환된 그리드 좌표
     {
+        if(background != null)
+        {
+            return (Vector2Int)background.WorldToCell(worldPos);
+        }
         return new Vector2Int((int)worldPos.x, (int)worldPos.y);
     }
 
     public Vector2 GriodToWorld(Vector2Int gridPos) // 그리드 좌표를 월드 좌표로 변경해주는 함수 / girdPos : 그리드 좌표, return : 변환된 월드 좌표
     {
+        if(background != null)
+        {
+            return background.CellToWorld((Vector3Int)gridPos) + new Vector3(0.5f, 0.5f);
+        }
         return new Vector2(gridPos.x + 0.5f, gridPos.y + 0.5f);
     }
 
@@ -88,19 +123,19 @@ public class GridMap
         // index = x + y * width
 
         // 왼쪽 아래가 원점일 때
-        // index = x + (height - y - 1) * widthS
+        // index = x + (height - y - 1) * width
 
         int index = Error_Not_Vaild_Position;
         if(IsValidPosition(x, y))
         {
-            index = x + (height - y - 1) * width;
+            index = (x - origin.x) + ((height - 1) - (y - origin.y)) * width;
         }
         return index;
     }
 
     public bool IsValidPosition(int x, int y)
     {// 입력받은 위치가 맵 내부인지 아닌지 확인하는 함수 / x : x좌표, y : y좌표, return : 맵 내무면 true, 맵 밖이면 flase
-        return x >= 0 && x < width && y >= 0 && y < height;
+        return x >= origin.x && x < (width + origin.x) && y >= origin.y && y < (height + origin.y);
     }
     public bool IsValidPosition(Vector2Int gridPos)
     {// 입력받은 위치가 맵 내부인지 아닌지 확인하는 함수 / x : x좌표, y : y좌표, return : 맵 내무면 true, 맵 밖이면 flase
--- Assets/Scripts/Test/Test_AstarTilemap.cs
+++ Assets/Scripts/Test/Test_AstarTilemap.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 
 public class Test_AstarTilemap : Test_Base
@@ -8,10 +9,27 @@ public class Test_AstarTilemap : Test_Base
     public Tilemap background;
     public Tilemap obstacle;
 
+    public Transform start;
+    public Transform end;
+
+    GridMap map;
+
     private void Start()
     {
+        map = new GridMap(background,obstacle);
+    }
+    protected override void Test1(InputAction.CallbackContext _)
+    {
+        Vector2Int startGrid = map.WorldToGrid(start.position);
+        Debug.Log($"Start : {startGrid}");
+        Vector2Int endGrid = map.WorldToGrid(end.position);
+        Debug.Log($"End : {endGrid}");
+    }
+
+    private void Test_GetTileMapInfos()
+    {
         // 타일맵의 크기
-        Debug.Log($"Background: {background.size}");    
+        Debug.Log($"Background: {background.size}");
         Debug.Log($"Obstacle: {obstacle.size}");
 
         // 타일맵의 워점(그리드 좌표)
@@ -19,12 +37,12 @@ public class Test_AstarTilemap : Test_Base
         Debug.Log($"Obstacle origin: {obstacle.origin}");
 
         // 타일맵 전체 순회하기
-        for(int y = background.cellBounds.yMin; y < background.cellBounds.yMax; y++ )
+        for (int y = background.cellBounds.yMin; y < background.cellBounds.yMax; y++)
         {
-            for(int x = background.cellBounds.xMin; x < background.cellBounds.xMax; x++)
+            for (int x = background.cellBounds.xMin; x < background.cellBounds.xMax; x++)
             {
                 TileBase tile = obstacle.GetTile(new(x, y));   // 특정 위치의 타일 가져오기
-                if(tile != null)
+                if (tile != null)
                 {
                     Debug.Log($"Obstacle Pos : ({x}, {y})");
                 }
